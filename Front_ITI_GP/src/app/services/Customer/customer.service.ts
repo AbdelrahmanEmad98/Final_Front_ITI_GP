5fa28daa4698adf3329bf97e0244a7f8386ca30e
Mahmoud Khaled <mahmoudxkhaled@gmail.com>
2023-07-05 17:46:13 +0300

    Revert "login_res_resetV1"
    This reverts commit abe9ed1cf3c655c0688b5e89ba1f481e75d13b6f.

--- Front_ITI_GP/src/app/services/Customer/customer.service.ts
+++ Front_ITI_GP/src/app/services/Customer/customer.service.ts
@@ -7,17 +7,7 @@ import { Injectable } from '@angular/core';
 export class CustomerService {
   constructor(private http: HttpClient) {}
 
-  private readonly API_URL="https://localhost:7052/api/"
   GetCustomerInfoById(CustomerId: any) {
     return this.http.get(`https://localhost:7052/api/Customer/${CustomerId}`);
-  }
-  AddCustomer(data: any){
-    return this.http.post(this.API_URL+"Security/Register", data);
-  }
-  LoginCustomer(data: any){
-    return this.http.post(this.API_URL+"Security/Login", data);
-  }
-  ResetPassword(data: any){
-    return this.http.patch(this.API_URL+"Customer/RestPassword", data);
   }
 }

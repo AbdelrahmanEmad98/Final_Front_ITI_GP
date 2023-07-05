a94b586207962de228ece9f40558ad27f1f128f4
Seif AbdelAziz <seif_abdelaziz@outlook.com>
2023-07-05 19:50:37 +0300

    Product Update V1

--- Front_ITI_GP/src/app/app-routing.module.ts
+++ Front_ITI_GP/src/app/app-routing.module.ts
@@ -8,18 +8,21 @@ import { RegisterComponent } from './components/register/register.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ErrorComponent } from './components/error/error.component';
 import { CheckOutComponent } from './components/check-out/check-out.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'category/:id', component: SubCategoryComponent },
+  { path: 'category/:id', component: ProductsComponent },
   { path: 'cart/:id', component: CartComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'aboutUs', component: AboutUsComponent },
   { path: 'cart', component: CartComponent },
-  { path:'checkout',component: CheckOutComponent},
-  { path: '**', component: ErrorComponent }
+  { path: 'checkout', component: CheckOutComponent },
+  { path: 'products/Details/:id', component: ProductDetailsComponent },
+  { path: '**', component: ErrorComponent },
 ];
 
 @NgModule({

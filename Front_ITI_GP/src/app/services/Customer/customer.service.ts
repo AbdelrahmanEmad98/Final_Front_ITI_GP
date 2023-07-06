08394ab73359ece10c4975c8c9625bfbe5af187d
Mahmoud Khaled <mahmoudxkhaled@gmail.com>
2023-07-06 10:56:10 +0300

    7-6-2023

--- Front_ITI_GP/src/app/services/Customer/customer.service.ts
+++ Front_ITI_GP/src/app/services/Customer/customer.service.ts
@@ -7,7 +7,16 @@ import { Injectable } from '@angular/core';
 export class CustomerService {
   constructor(private http: HttpClient) {}
 
-  GetCustomerInfoById(CustomerId: any) {
+  GetCustomerInfoById(CustomerId = '14a547f2-3abc-4854-aaec-2c775c5ce1cb') {
+    // return this.http.get(`https://localhost:7052/api/Customer/${CustomerId}`);
     return this.http.get(`https://localhost:7052/api/Customer/${CustomerId}`);
+  }
+
+  UpdateCustomerByID(customerUpdate: any) {
+    console.log(customerUpdate);
+    return this.http.patch(
+      `https://localhost:7052/api/Customer/14a547f2-3abc-4854-aaec-2c775c5ce1cb`,
+      customerUpdate
+    );
   }
 }
